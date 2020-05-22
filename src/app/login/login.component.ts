0b20401106af833eb08505bd9bd4e932c43e87be
christiandavbid06 <christiandavbid@gmail.com>
2020-05-22 01:31:55 -0500

    Primer commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css' ] //forma correcta
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
